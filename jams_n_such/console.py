8d94f0732b281826ee13fb0031013d3d656df2ab
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-02-05 14:34:41 +0000

    more html and figma update

--- jams_n_such/console.py
+++ jams_n_such/console.py
@@ -10,16 +10,23 @@ vendor_repository.delete_all()
 
 vendor1 = Vendor("Toby", "Toad")
 vendor_repository.save(vendor1)
-# Vendor2 = Vendor("Victor", "McDade")
-# vendor_repository.save(Vendor2)
+vendor2 = Vendor("Earl", "Grey")
+vendor_repository.save(vendor2)
+vendor3 = Vendor("Rupert", "")
+vendor_repository.save(vendor3)
+vendor4 = Vendor("Winnie", "")
+vendor_repository.save(vendor4)
 
 vendor_repository.select_all()
 
-product1 = Product("strawberry jam", "delicious", 1, 3, 10, vendor1)
+product1 = Product("Strawberry Jam", "Delicious", 1, 3, 10, vendor1)
 product_repository.save(product1)
-
-# product2 = Product("Go for a run", vendor1, 30, True)
-# product_repository.save(product2)
+product2 = Product("Blueberry Jam", "Delicious", 1, 3, 10, vendor1)
+product_repository.save(product2)
+product3 = Product("Apricot Jam", "Delicious", 1, 3, 10, vendor1)
+product_repository.save(product3)
+product4 = Product("Apple Jam", "Delicious", 1, 3, 10, vendor1)
+product_repository.save(product4)
 
 
 pdb.set_trace()
